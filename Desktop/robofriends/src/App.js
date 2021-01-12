5d709fe29d052e47591337c9051b2558ac60a4b7
Paul Yi <pauljsyi@gmail.com>
2021-01-11 23:02:07 -0800

    App.css

--- Desktop/robofriends/src/App.js
+++ Desktop/robofriends/src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 class App extends React.Component {
   render() {
     return     (
-    <div>
+    <div className="App">
       
       <h1>{this.props.greeting}</h1>
         <h3>Ninja says: {this.props.yell}</h3>
